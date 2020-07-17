c137e7535676b000c5ae1e8483c94c817c08af6b
Walid Budgaga <wbudgaga@gmail.com>
2020-07-17 13:19:03 -0600

    Update SorterByPublicationDate.java

--- src/cs518/a2/budgaga/sorter/SorterByPublicationDate.java
+++ src/cs518/a2/budgaga/sorter/SorterByPublicationDate.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs518.a2.budgaga.sorter;
 
 import cs518.a2.budgaga.core.BibtexEntry;
@@ -15,9 +11,9 @@ public class SorterByPublicationDate extends Sorter{
  
     @Override
     protected String getValue(BibtexEntry entry){
-        int month = entry.getMonth(); 
-        if (month<10)
-            return entry.getYear()+"0"+month;
-        return entry.getYear()+""+month;
+        int month      = entry.getMonth(); 
+        if (month < 10)
+            return entry.getYear() + "0" + month;
+        return entry.getYear() + "" + month;
     }
 }
