69eaad781a2d0fce1a51325e1b3affa4ea452970
Walid Budgaga <wbudgaga@gmail.com>
2020-07-17 13:16:28 -0600

    Update SorterByJournal.java

--- src/cs518/a2/budgaga/sorter/SorterByJournal.java
+++ src/cs518/a2/budgaga/sorter/SorterByJournal.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cs518.a2.budgaga.sorter;
 
 import cs518.a2.budgaga.core.BibtexEntry;
