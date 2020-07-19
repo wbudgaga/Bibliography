cf1436cf4ffc731ec517f079082a40727545e465
Walid Budgaga <wbudgaga@gmail.com>
2020-07-19 13:32:20 -0600

    Update HelperClass.java

--- src/cs518/a2/budgaga/util/HelperClass.java
+++ src/cs518/a2/budgaga/util/HelperClass.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs518.a2.budgaga.util;
 
 import java.util.*;
@@ -12,8 +8,8 @@ import java.util.*;
  */
 public class HelperClass {
   private static final  String[] MONTHS = {"January","February","March","April",
-                                     "May","June","July","August", "September",
-                                     "October","November","December"};
+                                            "May","June","July","August", "September",
+                                            "October","November","December"};
 
    public static int getMonthPos(String monthName){
        for (int i = 0; i < 12 ; ++i)
