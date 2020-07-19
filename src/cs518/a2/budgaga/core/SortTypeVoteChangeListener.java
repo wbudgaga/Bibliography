b83bd1b91aa7ff9f3e26309bfb13cdaa6874b2c7
Walid Budgaga <wbudgaga@gmail.com>
2020-07-19 13:34:39 -0600

    Update SortTypeVoteChangeListener.java

--- src/cs518/a2/budgaga/core/SortTypeVoteChangeListener.java
+++ src/cs518/a2/budgaga/core/SortTypeVoteChangeListener.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs518.a2.budgaga.core;
 
 import java.beans.PropertyChangeEvent;
@@ -16,7 +12,7 @@ public class SortTypeVoteChangeListener  implements VetoableChangeListener {
 
     @Override
     public void vetoableChange(PropertyChangeEvent evt) throws PropertyVetoException {
-       if (evt.getNewValue()==false)
+       if (evt.getNewValue() == false)
            throw new PropertyVetoException(evt.getPropertyName()+": False setting is not allowed", evt);
        ((Bibliography)evt.getOldValue()).reset();
     }
